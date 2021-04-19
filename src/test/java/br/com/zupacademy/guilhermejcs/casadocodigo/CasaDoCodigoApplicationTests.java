5ccd0729289130964385f5163c95a368e2ea7849
Guilherme Souza <guilhermejcamargos@gmail.com>
2021-04-19 12:25:34 -0300

    Comite inicial

---
+++ src/test/java/br/com/zupacademy/guilhermejcs/casadocodigo/CasaDoCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.guilhermejcs.casadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaDoCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
